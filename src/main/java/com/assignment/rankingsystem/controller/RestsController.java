e81944319776e78e2fe6ead50ef7efb9f42e7dd4
Merlin Chacko <Merlin Chacko>
2017-09-21 18:15:25 +0200

    commit#1

--- src/main/java/com/assignment/rankingsystem/controller/RestsController.java
+++ src/main/java/com/assignment/rankingsystem/controller/RestsController.java
@@ -1,9 +1,13 @@
 package com.assignment.rankingsystem.controller;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,44 @@ public class RestsController {
 
 	@RequestMapping(value = "/getallplayers", method = RequestMethod.GET)
 	public List<Players> getResource() {
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+
+			ClassPathResource resource = new ClassPathResource("names.txt");
+			fr = new FileReader(resource.getFile());
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+
 		
 		List<Players> list = new ArrayList<Players>();
 
