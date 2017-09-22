b633a9cd29bbf9f5c0a24ce553236d6085043e6c
Merlin Chacko <Merlin Chacko>
2017-09-22 18:20:26 +0200

    commit#2

--- src/main/java/com/assignment/rankingsystem/controller/RestsController.java
+++ src/main/java/com/assignment/rankingsystem/controller/RestsController.java
@@ -4,10 +4,13 @@ package com.assignment.rankingsystem.controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,24 +24,31 @@ import com.assignment.rankingsystem.dto.Players;
 @RestController
 public class RestsController {
 
-
+	@Value("classpath:files/names.txt") private Resource names;
+	@Value("classpath:files/matches.txt") private Resource matches;
 
 	@RequestMapping(value = "/getallplayers", method = RequestMethod.GET)
 	public List<Players> getResource() {
 		
 		BufferedReader br = null;
 		FileReader fr = null;
-
+		List<Players> list = new CopyOnWriteArrayList<Players>();
 		try {
 
-			ClassPathResource resource = new ClassPathResource("names.txt");
-			fr = new FileReader(resource.getFile());
+			fr = new FileReader(names.getFile());
 			br = new BufferedReader(fr);
 
-			String sCurrentLine;
+			String line;
 
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
+			while ((line = br.readLine()) != null) {
+				if(line.length() > 1) {
+				
+					String[] str = line.split("    ");
+					System.out.println( str[1]);
+					
+					Players p1 = new Players(Integer.parseInt(str[0]), str[1]);
+					list.add(p1);
+				}
 			}
 
 		} catch (IOException e) {
@@ -64,7 +74,7 @@ public class RestsController {
 		}
 
 		
-		List<Players> list = new ArrayList<Players>();
+		/*List<Players> list = new ArrayList<Players>();
 
 		Players p1 = new Players(1, "Masha");
 		Players p2 = new Players(2, "Richa");
@@ -75,8 +85,126 @@ public class RestsController {
 		list.add(p2);
 		list.add(p3);
 		list.add(p4);
+		*/
+
+		return fetchScore(list);
+	}
+	
+	private List<Players> fetchScore(List<Players> list) {
 		
+		Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
+		Map<Integer, Integer> winmap = new TreeMap<Integer, Integer>();
+		Map<Integer, Integer> losemap = new TreeMap<Integer, Integer>();
+		BufferedReader br = null;
+		FileReader fr = null;
+		
+		try {
+
+			fr = new FileReader(matches.getFile());
+			br = new BufferedReader(fr);
+
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				if(line.length() > 1) {
+				
+					String[] str = line.split(" ");
+					int id1 = Integer.parseInt(str[0]);
+					int id2 = Integer.parseInt(str[1]);
+					
+					if(winmap.containsKey(id1)) {
+						
+						winmap.put(id1, winmap.get(id1)+1);
+					}else {
+
+						winmap.put(id1, 1);
+					}
+					if(losemap.containsKey(id2)) {
+						
+						losemap.put(id2, losemap.get(id2)+1);
+					} else {
+
+						losemap.put(id2, 1);
+					}
+					
+
+					/*if(map.containsKey(id1)) {
+						
+						map.put(id1, map.get(id1)+1);
+					}else {
+
+						map.put(id1, 1);
+					}
+					if(map.containsKey(id2)) {
+						
+						map.put(id2, map.get(id2)+1);
+					} else {
+
+						map.put(id2, 1);
+					}*/
+				}
+			}
+			
+			for(Players player : list) {
+
+				for (Map.Entry<Integer, Integer> winentry : winmap.entrySet())
+				{
+					for (Map.Entry<Integer, Integer> loseentry : losemap.entrySet()) {
+
+						if(winentry.getKey() == loseentry.getKey()) {
+
+							if(winentry.getKey() == player.getId()) {
+
+								player.setScore(winentry.getValue()+loseentry.getValue());
+								player.setNoofwins(winentry.getValue());
+								player.setNoofloses(loseentry.getValue());
+							}
+						}else {
+							if(loseentry.getKey() == player.getId()) {
+								player.setScore(loseentry.getValue());
+								player.setNoofwins(0);
+								player.setNoofloses(loseentry.getValue());
+							}
+						}
+					}
+
+					/*if(winentry.getKey() == player.getId()) {
+
+						player.setScore(winentry.getValue());
+						player.setNoofwins(winentry.getValue());
+						player.setNoofloses(0);
+					}*/
+				}
+			}
+			
+			
+			
+			
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
 		return list;
+
+		
 	}
 
 }
--- src/main/java/com/assignment/rankingsystem/dto/Players.java
+++ src/main/java/com/assignment/rankingsystem/dto/Players.java
@@ -6,28 +6,60 @@ package com.assignment.rankingsystem.dto;
  */
 public class Players {
 	
-	private int id;
+	private Integer id;
 	private String name;
+	private Integer score;
+	private Integer noofwins;
+	private Integer noofloses;
 	
 	public Players() {}
 	
-	public Players(int id, String name) {
+	public Players(Integer id, String name) {
 		
 		this.id = id;
 		this.name = name;
 	}
-	
-	public int getId() {
+
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public Integer getNoofwins() {
+		return noofwins;
+	}
+
+	public void setNoofwins(Integer noofwins) {
+		this.noofwins = noofwins;
+	}
+
+	public Integer getNoofloses() {
+		return noofloses;
+	}
+
+	public void setNoofloses(Integer noofloses) {
+		this.noofloses = noofloses;
+	}
+	
+	
 	
 }
